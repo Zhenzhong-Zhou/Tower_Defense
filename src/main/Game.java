bf3e42eb6d260e8e83d5f9594e9963847e7549db
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-14 21:46:46 -0600

    Initial setup

---
+++ src/main/Game.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Game {
+    public Game() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start of my Tower Defense. Hello world!");
+    }
+}
