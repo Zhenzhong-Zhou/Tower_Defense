46f97527a992a82e50cb9ebdde76f485a03e227e
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-16 23:53:03 -0600

    Add rotation methods and more sprites

---
+++ src/helperMethods/ImageFix.java
@@ -0,0 +1,60 @@
+package helperMethods;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ImageFix {
+    // Rotate
+    public static BufferedImage getRotateImage(BufferedImage image, int rotAngle) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        BufferedImage newImage = new BufferedImage(width, height, image.getType());
+        Graphics2D graphics2D = newImage.createGraphics();
+
+        graphics2D.rotate(Math.toRadians(rotAngle), width/ 2, height / 2);
+        graphics2D.drawImage(image, 0, 0 , null);
+        graphics2D.dispose();
+
+        return newImage;
+    }
+
+    // Image layer build
+    public static BufferedImage buildImage(BufferedImage[] images) {
+        int width = images[0].getWidth();
+        int height = images[0].getHeight();
+
+        BufferedImage newImage = new BufferedImage(width, height, images[0].getType());
+        Graphics2D graphics2D = newImage.createGraphics();
+
+        for(BufferedImage image : images) {
+            graphics2D.drawImage(image, 0, 0 , null);
+        }
+        graphics2D.dispose();
+
+        return newImage;
+    }
+
+    // Rotate second image only
+    public static BufferedImage getBuildRotateImage(BufferedImage[] images, int rotAngle, int rotAtIndex) {
+        int width = images[0].getWidth();
+        int height = images[0].getHeight();
+
+        BufferedImage newImage = new BufferedImage(width, height, images[0].getType());
+        Graphics2D graphics2D = newImage.createGraphics();
+
+       for(int i = 0; i < images.length; i++) {
+           if(rotAtIndex == i) {
+               graphics2D.rotate(Math.toRadians(rotAngle), width/ 2, height / 2);
+           }
+           graphics2D.drawImage(images[i], 0, 0 , null);
+           if(rotAtIndex == i) {
+               graphics2D.rotate(Math.toRadians(-rotAngle), width/ 2, height / 2);
+           }
+
+       }
+       graphics2D.dispose();
+
+       return newImage;
+    }
+}
--- src/managers/TileManager.java
+++ src/managers/TileManager.java
@@ -1,5 +1,6 @@
 package managers;
 
+import helperMethods.ImageFix;
 import helperMethods.LoadSave;
 import objects.Tile;
 
@@ -7,7 +8,7 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class TileManager {
-    public Tile GRASS, WATER, ROAD;
+    public Tile GRASS, WATER, ROAD, BL_WATER_CORNER, TL_WATER_CORNER;
     public BufferedImage atlas;
     public ArrayList<Tile> tiles = new ArrayList<>();
 
@@ -18,9 +19,18 @@ public class TileManager {
 
     private void createTiles() {
         int id = 0;
-        tiles.add(GRASS = new Tile(getSprite(8, 1), id++, "Grass"));
-        tiles.add(WATER = new Tile(getSprite(0, 6), id++, "Water"));
-        tiles.add(ROAD = new Tile(getSprite(9, 0), id++, "Road"));
+        tiles.add(GRASS = new Tile(getSprite(9, 0), id++, "Grass"));
+        tiles.add(WATER = new Tile(getSprite(0, 0), id++, "Water"));
+        tiles.add(ROAD = new Tile(getSprite(8, 0), id++, "Road"));
+        tiles.add(BL_WATER_CORNER = new Tile(ImageFix.buildImage(getImages(0, 0, 5, 0)), id++, "BL_Water_Corner"));
+        tiles.add(TL_WATER_CORNER = new Tile(ImageFix.getBuildRotateImage(getImages(0, 0, 5, 0), 90, 1), id++, "TLL_Water_Corner"));
+    }
+
+    private BufferedImage[] getImages(int firstX, int firstY, int secondX, int secondY) {
+        return new BufferedImage[] {
+                getSprite(firstX, firstY),
+                getSprite(secondX, secondY)
+        };
     }
 
     private void loadAtlas() {
